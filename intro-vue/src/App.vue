3f90341df110f097c652f51e8b7c235a61283e88
Jimmy Castilho <jimmycastilho555@gmail.com>
2025-08-06 10:18:18 -0300

    feat: add Exemplo01.3Exercise component and integrate it into App.vue

--- intro-vue/src/App.vue
+++ intro-vue/src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 
 import Exemplo01 from './components/Exemplo01intepolacao.vue'
+import Exemplo01 from './components/Exemplo01.1Exercise.vue'
+import Exemplo01 from './components/Exemplo01.2Exercise.vue'
+import Exemplo01 from './components/Exemplo01.3Exercise.vue'
 
 </script>
 
---
+++ intro-vue/src/components/Exemplo01.3Exercise.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Ano: {{ dataAtual.getFullYear() }}</p>
+    <p>Mês: {{ dataAtual.getMonth() + 1 }}</p>
+    <p>Dia: {{ dataAtual.getDate() }}</p>
+    <p>Data completa: {{ dataAtual.toLocaleDateString() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dataAtual: new Date()
+    }
+  }
+}
+</script>
