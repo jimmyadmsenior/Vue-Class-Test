55e6d65281fe798ffc308d12d725bd203aa69160
Jimmy Castilho <jimmycastilho555@gmail.com>
2025-08-06 09:59:20 -0300

    feat: create Exemplo01Intepolacao component and integrate it into App.vue

--- intro-vue/src/App.vue
+++ intro-vue/src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 
+import Exemplo01 from './components/Exemplo01intepolacao.vue'
+
 </script>
 
 <template>
+
+<Exemplo01></Exemplo01>
 
 </template>
---
+++ intro-vue/src/components/Exemplo01Intepolacao.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+let nome = 'Jimmy'
+
+let num1 = 20
+
+let num2 = 20
+
+</script>
+
+
+<template>
+
+<p>{{ `O seu nome é: ${nome}` }}</p>
+
+<p>{{ num1 + num2 }}</p>
+
+</template>
