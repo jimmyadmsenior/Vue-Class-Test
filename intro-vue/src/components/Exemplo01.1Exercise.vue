c00eedafdb6969c27282e14e5fbcdb9b61944201
Jimmy Castilho <jimmycastilho555@gmail.com>
2025-08-06 10:10:10 -0300

    feat: add Exemplo01.1Exercise and Exemplo01.2Exercise components; remove Exemplo01Exercise component

---
+++ intro-vue/src/components/Exemplo01.1Exercise.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Meu nome é {{ nome }}, tenho {{ idade }} anos e sou {{ profissao }}.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: 'Jimmy',
+      idade: 18,
+      profissao: 'Cybersecurity Engineer'
+    }
+  }
+}
+</script>
---
+++ intro-vue/src/components/Exemplo01.2Exercise.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Soma: {{ a + b }}</p>
+    <p>Subtração: {{ a - b }}</p>
+    <p>Multiplicação: {{ a * b }}</p>
+    <p>Divisão: {{ a / b }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: 15,
+      b: 25
+    }
+  }
+}
+</script>
